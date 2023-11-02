157bbb7c390cb6793c9c4009b01f1608eaa7377c
Clément Côme <clement.come98@gmail.com>
2023-11-02 22:11:07 +0100

    Improve linting

--- cc_tk/util/types.py
+++ cc_tk/util/types.py
@@ -4,5 +4,6 @@ from typing import Union
 import numpy as np
 import pandas as pd
 
+# pylint: disable=C0103
 ArrayLike1D = Union[np.ndarray, pd.Series]
 ArrayLike2D = Union[np.ndarray, pd.DataFrame]
