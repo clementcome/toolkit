111cc53003baefde937c4262261ca1bd60fb5ed5
Clément Côme <clement.come98@gmail.com>
2023-11-23 23:52:18 +0100

    Read The Docs Documentation theme

--- docs_preparation/conf.py
+++ docs_preparation/conf.py
@@ -39,6 +39,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-extensions.append("sphinx_wagtail_theme")
-html_theme = "sphinx_wagtail_theme"
+# extensions.append("sphinx_wagtail_theme")
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
