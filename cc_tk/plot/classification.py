35c044f3e33978fad9dc63946a1735588e5faf2e
Clément Côme <clement.come98@gmail.com>
2023-11-26 22:05:01 +0100

    Add plots to documentation

--- cc_tk/plot/classification.py
+++ cc_tk/plot/classification.py
@@ -40,6 +40,16 @@ def plot_confusion(
         maximum value of each heatmap).
         If a tuple, the range is set to the given values.
         Default is 'global'.
+
+    Examples
+    --------
+    .. plot::
+        :include-source:
+
+        >>> from cc_tk.plot.classification import plot_confusion
+        >>> import numpy as np
+        >>> confusion_matrix = np.array([[15, 3, 1], [2, 10, 0], [0, 0, 5]])
+        >>> plot_confusion(confusion_matrix, fmt=".2f")
     """
     n = confusion_matrix.shape[0]
 
--- docs_preparation/conf.py
+++ docs_preparation/conf.py
@@ -30,6 +30,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.githubpages",
     "sphinx.ext.viewcode",
+    "matplotlib.sphinxext.plot_directive",
 ]
 
 templates_path = ["_templates"]
