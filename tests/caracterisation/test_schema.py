968c052829842cae2e49d5396be7d4fe38690050
Clément Côme <clement.come98@gmail.com>
2023-12-15 16:58:52 +0100

    Custom decorators for validating input Series

--- tests/caracterisation/test_schema.py
+++ tests/caracterisation/test_schema.py
@@ -1,3 +1,5 @@
+import inspect
+
 import pandas as pd
 import pandera as pa
 import pytest
@@ -5,6 +7,10 @@ import pytest
 from cc_tk.caracterisation.schema import (
     OnlyCategoricalSchema,
     OnlyNumericSchema,
+    SeriesType,
+    check_input_index,
+    check_input_types,
+    check_series_in_signature,
 )
 
 
@@ -44,3 +50,110 @@ class TestOnlyCategoricalSchema:
     def test_invalid_dataframe(self, invalid_dataframe):
         with pytest.raises(pa.errors.SchemaError):
             OnlyCategoricalSchema.validate(invalid_dataframe)
+
+
+class TestCheckSeriesInSignature:
+    def test_check_series_in_signature_valid(self):
+        def func(arg1: pd.Series, arg2: pd.Series):
+            pass
+
+        signature = check_series_in_signature(func, "arg1", "arg2")
+        assert isinstance(signature, inspect.Signature)
+
+    def test_check_series_in_signature_invalid_arg_name(self):
+        def func(arg1: pd.Series, arg2: pd.Series):
+            pass
+
+        with pytest.raises(ValueError):
+            check_series_in_signature(func, "arg1", "arg3")
+
+    def test_check_series_in_signature_invalid_arg_type(self):
+        def func(arg1: pd.Series, arg2: int):
+            pass
+
+        with pytest.raises(TypeError):
+            check_series_in_signature(func, "arg1", "arg2")
+
+
+class TestCheckInputTypesDecoration:
+    def test_check_input_types_decoration_valid(self):
+        @check_input_types(
+            ("arg1", SeriesType.NUMERIC), ("arg2", SeriesType.CATEGORICAL)
+        )
+        def dummy_function(arg1: pd.Series, arg2: pd.Series, arg3: int):
+            pass
+
+    def test_check_input_types_decoration_invalid_arg(self):
+        with pytest.raises(ValueError):
+
+            @check_input_types(
+                ("arg1", SeriesType.NUMERIC), ("arg4", SeriesType.CATEGORICAL)
+            )
+            def dummy_function(arg1: pd.Series, arg2: pd.Series, arg3: int):
+                pass
+
+    def test_check_input_types_decoration_invalid_type(self):
+        with pytest.raises(TypeError):
+
+            @check_input_types(
+                ("arg1", SeriesType.NUMERIC), ("arg3", SeriesType.CATEGORICAL)
+            )
+            def dummy_function(arg1: pd.Series, arg2: pd.Series, arg3: int):
+                pass
+
+
+class TestCheckInputTypes:
+    @staticmethod
+    @check_input_types(
+        ("arg1", SeriesType.NUMERIC), ("arg2", SeriesType.CATEGORICAL)
+    )
+    def dummy_function(arg1: pd.Series, arg2: pd.Series):
+        pass
+
+    @pytest.fixture
+    def numeric_series(self):
+        return pd.Series([1, 2, 3])
+
+    @pytest.fixture
+    def categorical_series(self):
+        return pd.Series(["a", "b", "c"])
+
+    def test_check_input_types_valid(self, numeric_series, categorical_series):
+        self.dummy_function(numeric_series, categorical_series)
+
+    def test_check_input_types_invalid_numeric(self, categorical_series):
+        invalid_numeric_series = pd.Series([1, 2, 3, "a"])
+        with pytest.raises(TypeError):
+            self.dummy_function(invalid_numeric_series, categorical_series)
+
+    def test_check_input_types_invalid_categorical(self, numeric_series):
+        invalid_categorical_series = numeric_series.copy()
+        with pytest.raises(TypeError):
+            self.dummy_function(numeric_series, invalid_categorical_series)
+
+
+class TestCheckInputIndex:
+    @staticmethod
+    @check_input_index("arg1", "arg2")
+    def dummy_function(arg1: pd.Series, arg2: pd.Series):
+        pass
+
+    @pytest.fixture
+    def series_with_same_index(self):
+        index = pd.Index([1, 2, 3])
+        return pd.Series([1, 2, 3], index=index)
+
+    @pytest.fixture
+    def series_with_different_index(self):
+        index1 = pd.Index([1, 2, 3])
+        index2 = pd.Index([4, 5, 6])
+        return pd.Series([1, 2, 3], index=index1), pd.Series(
+            [4, 5, 6], index=index2
+        )
+
+    def test_check_input_index_valid(self, series_with_same_index):
+        self.dummy_function(series_with_same_index, series_with_same_index)
+
+    def test_check_input_index_invalid(self, series_with_different_index):
+        with pytest.raises(ValueError):
+            self.dummy_function(*series_with_different_index)
